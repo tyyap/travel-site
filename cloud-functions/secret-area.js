02b6530fe64dbc00c9e199769e7bf182f65bdb74
Yap Tiam Yee <mp3yty@hotmail.com>
2022-10-06 17:19:54 +0800

    updated cloud function

--- cloud-functions/secret-area.js
+++ cloud-functions/secret-area.js
@@ -1,26 +1,27 @@
-exports.handler = function (event, context, callback) {
+exports.handler = function(event, context, callback) {
     const secretContent = `
     <h3>Welcome To The Secret Area</h3>
-    <p>Here we can tell you that the sky is <strong>blue</strong></p>
+    <p>Here we can tell you that the sky is <strong>blue</strong>, and two plus two equals four.</p>
     `
-    let body;
-
-    if(event.body) {
-        body = JSON.parse(event.body);
-    } else {
-        body = {}
-    }
-
-    if(body.password == "javascript") {
-        callback(null, {
-            statusCode: 200,
-            body: secretContent
-        })
-    } else {
-        callback(null, {
-            statusCode: 401
-        })
-    }
     
-   
-}
+    let body
+  
+    if (event.body) {
+      body = JSON.parse(event.body)
+    } else {
+      body = {}
+    }
+  
+    if (body.password == "javascript") {
+      callback(null, {
+        statusCode: 200,
+        body: secretContent
+      })
+    } else {
+      callback(null, {
+        statusCode: 401
+      })
+    }
+  
+    
+  }
