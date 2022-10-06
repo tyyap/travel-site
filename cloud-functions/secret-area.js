fe96dd0cd15dfa78afe36479af943a7f1df8bb4a
Yap Tiam Yee <mp3yty@hotmail.com>
2022-10-06 17:33:01 +0800

    added cors solution to test cloud function from localhost

--- cloud-functions/secret-area.js
+++ cloud-functions/secret-area.js
@@ -1,4 +1,18 @@
 exports.handler = function(event, context, callback) {
+    
+    const headers = {
+        "Access-Control-Allow-Origin" : "*",
+        "Access-Control-Allow-Headers" : "Content-Type"
+    }
+
+    if (event.httpMethod !== "POST") {
+        return callback(null, {
+          statusCode: 200,
+          headers,
+          body: "This was not a POST request"
+        })
+      }
+
     const secretContent = `
     <h3>Welcome To The Secret Area</h3>
     <p>Here we can tell you that the sky is <strong>blue</strong>, and two plus two equals four.</p>
@@ -15,11 +29,13 @@ exports.handler = function(event, context, callback) {
     if (body.password == "javascript") {
       callback(null, {
         statusCode: 200,
+        headers,
         body: secretContent
       })
     } else {
       callback(null, {
-        statusCode: 401
+        statusCode: 401,
+        headers,
       })
     }
   
